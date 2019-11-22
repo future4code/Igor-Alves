9a05dd37e921d791b20ffee9c64fa714f2653f06
Igor Alves <igoralvesantos@gmail.com>
2019-11-22 16:32:29 -0300

    Realizei a etapa 6.

--- semana4/aula4-funcoes-de-array/controlador-de-despesas/script.js
+++ semana4/aula4-funcoes-de-array/controlador-de-despesas/script.js
@@ -6,7 +6,7 @@ class Despesa{
     }
 }
 
-const totalDespesas = []
+let totalDespesas = []
 
 function salvarDespesa() {
     const valor =document.getElementById("valor-cadastro")
@@ -24,8 +24,40 @@ function salvarDespesa() {
         tipo.value = ""
         descricao.value = ""
         console.log(totalDespesas)
+    }
+}
 
+let despesaFiltrada = []
+
+function filtrarDespesa() {
+    const type = document.getElementById("tipo-detalhes")
+    const valorMin = document.getElementById("valor-minimo")
+    const valorMax = document.getElementById("valor-maximo")
+
+    if (type.value === "" || valorMin.value === "" || valorMax.value === "") {
+        alert("Todos os campos devem ser preenchidos e o valor da despesa deve ser do tipo númerico.")
+    } else {
+        despesaFiltrada = totalDespesas.filter((totalDespesas) => {
+            return totalDespesas.tipo === type.value && totalDespesas.valor >= valorMin.value && totalDespesas.valor <= valorMax.value
+        })
+    }
+
+    console.log(despesaFiltrada)
+    mostrarDespesas(despesaFiltrada)
+}
+
+let descritivo = document.getElementById("lista-despesas")
+
+function mostrarDespesas(despesaFiltrada) {
+    for (item of despesaFiltrada){
+        descritivo.innerHTML += "<p> Descrição: " + item.descricao + "</p>"
+        descritivo.innerHTML += "<p> Tipo: " + item.tipo + "</p>"
+        descritivo.innerHTML += "<p> Valor: R$"+ item.valor + "</p>"
     }
 }
 
 
+function valorTotal (){
+    let total = 0
+    for (num of )
+}
