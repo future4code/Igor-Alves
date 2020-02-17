f9fd4008396fecca870c7ff0d69e6f413c96fcae
Igor Alves <igoralvesantos@gmail.com>
2020-02-16 22:43:53 -0300

    Criação dos scripts no package.json e finalizei dois desafios

--- semana14/projeto-f4bank/src/createAccount.ts
+++ semana14/projeto-f4bank/src/createAccount.ts
@@ -2,19 +2,19 @@ import { readFile, writeFile } from 'fs'
 import * as moment from 'moment'
 moment.locale('pt-br')
 
-const databasePath = '../database.json' 
+const databasePath = './database.json' 
 
 type account = {
     name: string,
-    CPF: string,
-    birthDay: moment.Moment,
+    cpf: string,
+    birthDay: string,
     balance: number
     extract: expense[],
 }
 
 type expense = {
     value: number,
-    date: moment.Moment,
+    date: string,
     description: string,
 }
 
@@ -25,8 +25,8 @@ type accountsData = {
 
 const newAccount: account = {
     name: process.argv[2],
-    CPF: process.argv[3],
-    birthDay: moment(process.argv[4], 'DD/MM/YYYY'),
+    cpf: process.argv[3],
+    birthDay: moment(process.argv[4], 'DD/MM/YYYY').format('DD/MM/YYYY'),
     balance: 0,
     extract: [],
 }
@@ -50,13 +50,12 @@ const readDatabase: Promise<string> = new Promise((resolve, reject) => {
 })
 
 
-const createAccount = async () => {
-    const accountList = await getAllAccounts()
+const createAccount = async (accountList: accountsData) => {
     accountList.clientsAccount.push(newAccount);
     const allAccounts = JSON.stringify(accountList);
     
     const putClientsData: Promise<void> = new Promise((resolve, reject) => {
-        writeFile(databasePath, allAccounts, 'utf8', (err) => {
+        writeFile(databasePath, allAccounts, (err) => {
             if(err){
                 reject(err)
                 return
@@ -72,55 +71,64 @@ const createAccount = async () => {
 
 
 const verifyIsAdult = (): boolean => {
-    const currentDate: moment.Moment = moment().utcOffset('-0300')
-    const clientBirthDay: moment.Moment = newAccount.birthDay.utcOffset('-0300')
+    const currentDate: moment.Moment = moment()
+    const clientBirthDay: moment.Moment = moment(newAccount.birthDay, 'DD/MM/YYYY')
     const diffInYears: number = currentDate.diff(clientBirthDay, "years")
-    
+
     if(diffInYears >= 18) {
         return true
     } else {
         console.log(`
         -------------------------------------------------------------------
         O cliente precisa ter idade maior que 18 anos para abrir uma conta.
-        -------------------------------------------------------------------
-        `)
+        -------------------------------------------------------------------`)
         return false
     }
 }
 
 
-const verifyUniqueCPF = async () => {
+const verifyUniqueCPF = (accountList: accountsData): boolean => {
     let uniqueCPF: boolean
-    const accountList = await getAllAccounts()
 
-    accountList.clientsAccount.forEach( account => {
-        const databaseCPF: number = Number(account.CPF.replace(/\D+/g, ''))
-        const clientCPF: number = Number(process.argv[3].replace(/\D+/g, ''))
-        
-        if(clientCPF === databaseCPF){
-            uniqueCPF = true
-        }else{
-            uniqueCPF = false
-        }
-    })
-
-    if (uniqueCPF === false){
-        console.log(`
-        ----------------------------------------
-        Esse CPF já possui uma conta cadastrada.
-        ----------------------------------------`)
+    if(accountList.clientsAccount.length !== 0){
+        accountList.clientsAccount.forEach( account => {
+            const databaseCPF: number = Number(account.cpf.replace(/\D+/g, ''))
+            const clientCPF: number = Number(process.argv[3].replace(/\D+/g, ''))
+    
+            if(clientCPF !== databaseCPF){
+                uniqueCPF = true
+            }else{
+                console.log(`
+                ----------------------------------------
+                Esse CPF já possui uma conta cadastrada.
+                ----------------------------------------`)
+                uniqueCPF = false
+            }
+        })
+    } else{
+        uniqueCPF = true
     }
+
     return uniqueCPF
 }
 
 
-const main = async() => {
-    if (verifyIsAdult()) {
-        createAccount()
+const main = async () => {
+    const accountList = await getAllAccounts()
+
+    try {
+        const adult = verifyIsAdult();
+        const uniqueCPF = verifyUniqueCPF(accountList);
+
+        if(adult && uniqueCPF){
+            createAccount(accountList)
+        }
+    } catch {
+        console.log("Ocorreu um erro!")
     }
 }
 
-main()
 
+main()
 
 
